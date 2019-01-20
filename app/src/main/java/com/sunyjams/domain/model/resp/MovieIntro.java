c8f653ddcd82fe0e617f7bab91fa03739471ad1f
James <sunyjams@hotmail.com>
2019-01-20 11:50:41 +0800

    update

---
+++ app/src/main/java/com/sunyjams/domain/model/resp/MovieIntro.java
@@ -0,0 +1,20 @@
+package com.sunyjams.domain.model.resp;
+
+
+/**
+ * Created by James
+ * Date 2019/1/20.
+ * description 艺术家，包括导演和演员
+ */
+public class MovieIntro {
+
+    private RateInfo rating;
+
+    private Integer reviews_count;
+
+    private String[] videos;
+
+    private Integer wish_count;
+
+    private String original_title;
+}
